a4996d7d9b5ce761334951cce8db877f43cc94b4
William-Moreno <intrcptr@comcast.net>
2020-10-29 12:36:25 -0700

    added in functions to practice

--- js/app.js
+++ js/app.js
@@ -2,86 +2,98 @@
 window.onload = function() {
   var totalScore = 0;
   var numberOfQuestions = 7;
+  var userName;
 
-  var userName = prompt('Hello! What is your name?');
-  alert('It is nice to meet you, ' + userName + '. My name is Bill. Welcome to the site. Shall we start the game?');
-  // console.log('It is nice to meet you, ' + userName + '. My name is Bill. Welcome to the site. Shall we start the game?');
-  alert('Determine if the following statements are correct. Please answer the questions with "yes/y" or "no/n".');
-
-  // first question
-  var firstAnswer = prompt('Bill studied forensic science at the University of Washington.').toLowerCase();
-
-  if (firstAnswer === 'yes' || firstAnswer === 'y') {
-    alert('Sorry. That is incorrect. Bill studied forensic science at Green River College.');
-    // console.log('Sorry. That is incorrect. Bill studied forensic science at Green River College.');
-  } else if (firstAnswer === 'no' || firstAnswer === 'n') {
-    alert('Correct! Bill studied engineering at the University of Washington.');
-    // console.log('Correct! Bill studied engineering at the University of Washington.');
-    totalScore++;
-  } else {
-    alert('You did not answer "yes/y" or "no/n", so I am afraid you do not get credit. Please answer subsequent questions either "yes/y" or "no/n".');
-    // console.log('You did not answer "yes/y" or "no/n", so I am afraid you do not get credit. Please answer subsequent questions either "yes/y" or "no/n".');
+  function welcomeMessage(){
+    userName = prompt('Hello! What is your name?');
+    alert('It is nice to meet you, ' + userName + '. My name is Bill. Welcome to the site. Shall we start the game?');
+    // console.log('It is nice to meet you, ' + userName + '. My name is Bill. Welcome to the site. Shall we start the game?');
+    alert('Determine if the following statements are correct. Please answer the questions with "yes/y" or "no/n".');
   }
 
-  // second question
-  var secondAnswer = prompt('One of Bill\'s main goals is to become a software developer.').toLowerCase();
+  function firstQuestion(){
+    var firstAnswer = prompt('Bill studied forensic science at the University of Washington.').toLowerCase();
 
-  if (secondAnswer === 'no' || secondAnswer === 'n') {
-    alert('Sorry. That is incorrect. Bill does indeed aspire to becoming a successful software developer.');
-    // console.log('Sorry. That is incorrect. Bill does indeed aspire to becoming a successful software developer.');
-  } else if (secondAnswer === 'yes' || secondAnswer === 'y') {
-    alert('Right! Bill is currently working toward that very goal. (...at breakneck speed)');
-    // console.log('Right! Bill is currently working toward that very goal. (...at breakneck speed)');
-    totalScore++;
-  } else {
-    alert('You did not answer "yes/y" or "no/n", so I am afraid you do not get credit. Please answer subsequent questions either "yes/y" or "no/n".');
-    // console.log('You did not answer "yes/y" or "no/n", so I am afraid you do not get credit. Please answer subsequent questions either "yes/y" or "no/n".');
+    if (firstAnswer === 'yes' || firstAnswer === 'y') {
+      alert('Sorry. That is incorrect. Bill studied forensic science at Green River College.');
+      // console.log('Sorry. That is incorrect. Bill studied forensic science at Green River College.');
+    } else if (firstAnswer === 'no' || firstAnswer === 'n') {
+      alert('Correct! Bill studied engineering at the University of Washington.');
+      // console.log('Correct! Bill studied engineering at the University of Washington.');
+      totalScore++;
+    } else {
+      alert('You did not answer "yes/y" or "no/n", so I am afraid you do not get credit. Please answer subsequent questions either "yes/y" or "no/n".');
+      // console.log('You did not answer "yes/y" or "no/n", so I am afraid you do not get credit. Please answer subsequent questions either "yes/y" or "no/n".');
+    }
+  }
+  function secondQuestion(){
+    var secondAnswer = prompt('One of Bill\'s main goals is to become a software developer.').toLowerCase();
+
+    if (secondAnswer === 'no' || secondAnswer === 'n') {
+      alert('Sorry. That is incorrect. Bill does indeed aspire to becoming a successful software developer.');
+      // console.log('Sorry. That is incorrect. Bill does indeed aspire to becoming a successful software developer.');
+    } else if (secondAnswer === 'yes' || secondAnswer === 'y') {
+      alert('Right! Bill is currently working toward that very goal. (...at breakneck speed)');
+      // console.log('Right! Bill is currently working toward that very goal. (...at breakneck speed)');
+      totalScore++;
+    } else {
+      alert('You did not answer "yes/y" or "no/n", so I am afraid you do not get credit. Please answer subsequent questions either "yes/y" or "no/n".');
+      // console.log('You did not answer "yes/y" or "no/n", so I am afraid you do not get credit. Please answer subsequent questions either "yes/y" or "no/n".');
+    }
+  }
+  function thirdQuestion(){
+    var thirdAnswer = prompt('Bill\'s son, Riley, is his eldest child.').toLowerCase();
+
+    if (thirdAnswer === 'no' || thirdAnswer === 'n') {
+      alert('Unfortunately, that is incorrect. His son, Riley, is 14 while his daughter, Alexis, is only 12.');
+      // console.log('Unfortunately, that is incorrect. His son, Riley, is 14 while his daughter, Alexis, is only 12.');
+    } else if (thirdAnswer === 'yes' || thirdAnswer === 'y') {
+      alert('That is correct! Riley is actually only older than his sister by 17 months.');
+      // console.log('That is correct! Riley is actually only older than his sister by 17 months.');
+      totalScore++;
+    } else {
+      alert('You did not answer "yes/y" or "no/n", so I am afraid you do not get credit. Please answer subsequent questions either "yes/y" or "no/n".');
+      // console.log('You did not answer "yes/y" or "no/n", so I am afraid you do not get credit. Please answer subsequent questions either "yes/y" or "no/n".');
+    }
+  }
+  function fourthQuestion(){
+    var fourthAnswer = prompt('Bill spent 15 years working in the aerospace manufacturing industry.').toLowerCase();
+
+    if (fourthAnswer === 'yes' || fourthAnswer === 'y') {
+      alert('Sorry. That is incorrect. Bill spent 15 years working in the trucking manufacturing industry.');
+      // console.log('Sorry. That is incorrect. Bill spent 15 years working in the trucking manufacturing industry.');
+    } else if (fourthAnswer === 'no' || fourthAnswer === 'n') {
+      alert('Yes! Bill only worked in aerospace for 5 years.');
+      // console.log('Yes! Bill only worked in aerospace for 5 years.');
+      totalScore++;
+    } else {
+      alert('You did not answer "yes/y" or "no/n", so I am afraid you do not get credit. Please answer subsequent questions either "yes/y" or "no/n".');
+      // console.log('You did not answer "yes/y" or "no/n", so I am afraid you do not get credit. Please answer subsequent questions either "yes/y" or "no/n".');
+    }
+  }
+  function fifthQuestion(){
+    var fifthAnswer = prompt('So far, Bill has lived in Tacoma a total of 4 times.').toLowerCase();
+
+    if (fifthAnswer === 'yes' || fifthAnswer === 'y') {
+      alert('Fooled you! Bill technically only lived in Tacoma 3 times. Lakewood, a suburb of Tacoma, actually officially became a city in 1996. Tricksy!');
+      // console.log('Fooled you! Bill technically only lived in Tacoma 3 times. Lakewood, a suburb of Tacoma, actually officially became a city in 1996. Tricksy!');
+    } else if (fifthAnswer === 'no' || fifthAnswer === 'n') {
+      alert('Very good! Bill technically only lived in Tacoma 3 times. Lakewood, a suburb of Tacoma, actually officially became a city in 1996. Tricksy!');
+      // console.log('Very good! Bill technically only lived in Tacoma 3 times. Lakewood, a suburb of Tacoma, actually officially became a city in 1996. Tricksy!');
+      totalScore++;
+    } else {
+      alert('You did not answer "yes/y" or "no/n", so I am afraid you do not get credit. Please answer subsequent questions either "yes/y" or "no/n".');
+      // console.log('You did not answer "yes/y" or "no/n", so I am afraid you do not get credit. Please answer subsequent questions either "yes/y" or "no/n".');
+    }
   }
 
-  // third question
-  var thirdAnswer = prompt('Bill\'s son, Riley, is his eldest child.').toLowerCase();
+  welcomeMessage();
+  firstQuestion();
+  secondQuestion();
+  thirdQuestion();
+  fourthQuestion();
+  fifthQuestion();
 
-  if (thirdAnswer === 'no' || thirdAnswer === 'n') {
-    alert('Unfortunately, that is incorrect. His son, Riley, is 14 while his daughter, Alexis, is only 12.');
-    // console.log('Unfortunately, that is incorrect. His son, Riley, is 14 while his daughter, Alexis, is only 12.');
-  } else if (thirdAnswer === 'yes' || thirdAnswer === 'y') {
-    alert('That is correct! Riley is actually only older than his sister by 17 months.');
-    // console.log('That is correct! Riley is actually only older than his sister by 17 months.');
-    totalScore++;
-  } else {
-    alert('You did not answer "yes/y" or "no/n", so I am afraid you do not get credit. Please answer subsequent questions either "yes/y" or "no/n".');
-    // console.log('You did not answer "yes/y" or "no/n", so I am afraid you do not get credit. Please answer subsequent questions either "yes/y" or "no/n".');
-  }
-
-  // fourth question
-  var fourthAnswer = prompt('Bill spent 15 years working in the aerospace manufacturing industry.').toLowerCase();
-
-  if (fourthAnswer === 'yes' || fourthAnswer === 'y') {
-    alert('Sorry. That is incorrect. Bill spent 15 years working in the trucking manufacturing industry.');
-    // console.log('Sorry. That is incorrect. Bill spent 15 years working in the trucking manufacturing industry.');
-  } else if (fourthAnswer === 'no' || fourthAnswer === 'n') {
-    alert('Yes! Bill only worked in aerospace for 5 years.');
-    // console.log('Yes! Bill only worked in aerospace for 5 years.');
-    totalScore++;
-  } else {
-    alert('You did not answer "yes/y" or "no/n", so I am afraid you do not get credit. Please answer subsequent questions either "yes/y" or "no/n".');
-    // console.log('You did not answer "yes/y" or "no/n", so I am afraid you do not get credit. Please answer subsequent questions either "yes/y" or "no/n".');
-  }
-
-  // fifth question
-  var fifthAnswer = prompt('So far, Bill has lived in Tacoma a total of 4 times.').toLowerCase();
-
-  if (fifthAnswer === 'yes' || fifthAnswer === 'y') {
-    alert('Fooled you! Bill technically only lived in Tacoma 3 times. Lakewood, a suburb of Tacoma, actually officially became a city in 1996. Tricksy!');
-    // console.log('Fooled you! Bill technically only lived in Tacoma 3 times. Lakewood, a suburb of Tacoma, actually officially became a city in 1996. Tricksy!');
-  } else if (fifthAnswer === 'no' || fifthAnswer === 'n') {
-    alert('Very good! Bill technically only lived in Tacoma 3 times. Lakewood, a suburb of Tacoma, actually officially became a city in 1996. Tricksy!');
-    // console.log('Very good! Bill technically only lived in Tacoma 3 times. Lakewood, a suburb of Tacoma, actually officially became a city in 1996. Tricksy!');
-    totalScore++;
-  } else {
-    alert('You did not answer "yes/y" or "no/n", so I am afraid you do not get credit. Please answer subsequent questions either "yes/y" or "no/n".');
-    // console.log('You did not answer "yes/y" or "no/n", so I am afraid you do not get credit. Please answer subsequent questions either "yes/y" or "no/n".');
-  }
 
   /* guessing game */
 
